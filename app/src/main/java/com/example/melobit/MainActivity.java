36826b794c1a5d9c72113317158ebcddd6e53a59
soginim <sogandk317@gmail.com>
2023-01-20 18:19:42 +0330

    COMMIT2. DEPENDECIES ADD. MAIN PAGE XML

--- app/src/main/java/com/example/melobit/MainActivity.java
+++ app/src/main/java/com/example/melobit/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.melobit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView newhits2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        newhits2=(TextView) findViewById(R.id.newhits2);
+
+        newhits2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent activitylistintent = new Intent(getApplicationContext(), activitylist.class);
+                startActivity(activitylistintent);
+
+            }
+
+        });
+
     }
+
+
+
 }
