f5ebd387cee4710df8387ad2e69fa79427944afb
soginim <sogandk317@gmail.com>
2023-01-21 03:32:28 +0330

    COMMIT5. LAST ONE. ADDED MEDIA AND OPTIMIZED DESIGN

--- app/src/main/java/com/example/melobit/MainActivity.java
+++ app/src/main/java/com/example/melobit/MainActivity.java
@@ -1,15 +1,5 @@
 package com.example.melobit;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
-
-import com.google.android.exoplayer2.ExoPlayer;
-import com.google.android.exoplayer2.source.DefaultMediaSourceFactory;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
@@ -21,8 +11,10 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.example.melobit.R;
-
-
+import com.example.melobit.MusicInfo_M;
+import com.example.melobit.MusicRepository;
+import com.example.melobit.HomeFragment;
+import com.example.melobit.SearchFragment;
 import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.source.DefaultMediaSourceFactory;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -30,26 +22,33 @@ import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
+    BottomNavigationView bottomNavigationView;
     public static ExoPlayer exoPlayerAudio;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-
-
         setContentView(R.layout.activity_main);
-
-        exoPlayerAudio = new ExoPlayer.Builder(this).setMediaSourceFactory(new DefaultMediaSourceFactory(this)
-                        .setLiveTargetOffsetMs(3000)).build();
-
-
-
-
+        initBottomNavigationView();
+        exoPlayerAudio = new ExoPlayer.Builder(this).setMediaSourceFactory(new DefaultMediaSourceFactory(this).setLiveTargetOffsetMs(5000))
+                .build();
     }
 
-
-
+    private void initBottomNavigationView() {
+        HomeFragment firstFragment = new HomeFragment();
+        SearchFragment secondFragment = new SearchFragment();
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, firstFragment).commit();
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.home:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, firstFragment).commit();
+                    return true;
+                case R.id.search:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, secondFragment).commit();
+                    return true;
+            }
+            return false;
+        });
+    }
 }
